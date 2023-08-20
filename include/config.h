baf9ed7d900b979729dfe7b3cc00f5d7ecc4b19e
K4CZP3R <kacper@kacperserewis.net>
2023-08-21 01:13:39 +0200

    move config to config.h

---
+++ include/config.h
@@ -0,0 +1,11 @@
+#define PANEL_RES_X 32                 // Number of pixels wide of each INDIVIDUAL panel module.
+#define PANEL_RES_Y 16                 // Number of pixels tall of each INDIVIDUAL panel module.
+#define NUM_ROWS 2                     // Number of rows of chained INDIVIDUAL PANELS
+#define NUM_COLS 1                     // Number of INDIVIDUAL PANELS per ROW
+#define PANEL_CHAIN NUM_ROWS *NUM_COLS // total number of panels chained one to another
+#define VIRTUAL_MATRIX_CHAIN_TYPE CHAIN_BOTTOM_LEFT_UP
+#define SCREEN_WIDTH 32
+#define SCREEN_HEIGHT 32
+
+#define WIFI_SSID ""
+#define WIFI_PASS ""
--- include/main.h
+++ include/main.h
@@ -1,25 +1,10 @@
 #include <Arduino.h>
-#include <ESP32-VirtualMatrixPanel-I2S-DMA.h>
-#include <FastLED.h>
+#include "matrix.h"
+#include "cover-art.h"
+#include "serial-recv.h"
+#include "wireless.h"
+#include "socket.h"
 
-// #define USE_WS 1
+#define USE_WS 1
 
 #define BAUD_RATE 115200 // serial debug port baud rate
-
-#define PANEL_RES_X 32                 // Number of pixels wide of each INDIVIDUAL panel module.
-#define PANEL_RES_Y 16                 // Number of pixels tall of each INDIVIDUAL panel module.
-#define NUM_ROWS 2                     // Number of rows of chained INDIVIDUAL PANELS
-#define NUM_COLS 1                     // Number of INDIVIDUAL PANELS per ROW
-#define PANEL_CHAIN NUM_ROWS *NUM_COLS // total number of panels chained one to another
-#define VIRTUAL_MATRIX_CHAIN_TYPE CHAIN_BOTTOM_LEFT_UP
-#define SCREEN_WIDTH 32
-#define SCREEN_HEIGHT 32
-
-#ifdef USE_WS
-#include <WiFi.h>
-#include <AsyncTCP.h>
-#include <ESPAsyncWebServer.h>
-
-#define WIFI_SSID "PanicAtTheCiscoEXT"
-#define WIFI_PASS "aQkkVzctkrj6"
-#endif
